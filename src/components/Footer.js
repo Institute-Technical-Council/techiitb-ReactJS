7cf52d42149ac177ead3dfbe12e4ac95ec2daa4d
VishnuSanjevThulasiraman <52427253+VishnuSanjevThulasiraman@users.noreply.github.com>
2020-10-15 19:19:30 +0530

    made footer groups dynamic

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,10 @@ import '../style/footer.css'
 import {Group1, Group2, Group3, Group4} from '../components/FooterGroups'
 
 function Footer(props) {
-
+    var heading_of_group1 = "Explore";
+    var content_of_group1 = ["Mission", "Clubs and Team", "Documentations", "Careers"];
+    var heading_of_group4 = "Contact";
+    var content_of_group4 = ["Facebook", "Instagram", "LinkedIn"];
     return (
 
         <div className="row footer">
@@ -11,7 +14,7 @@ function Footer(props) {
             <h1>ITC Logo</h1>
             </div>
             <div className="col-md-3 sm-3">
-                <Group1 />
+                <Group1 heading={heading_of_group1} content={content_of_group1}/>
             </div>
             <div className="col-md-3 sm-3">
                 <Group2 />
@@ -21,7 +24,7 @@ function Footer(props) {
                     <Group3 />
                 </div>
                 <div className="row">
-                    <Group4 />
+                    <Group4 heading={heading_of_group4} content={content_of_group4}/>
                 </div>
             </div>
         </div>
--- src/components/FooterGroups.js
+++ src/components/FooterGroups.js
@@ -1,40 +1,30 @@
 import React from 'react';
 import '../style/footer.css'
 
-function Group1 (props) {
+function Group1(props) {
+    var heading = props.heading;
+    var objects = props.content;
     return (
         <div className="group1">
             <div className="row">
-                <h4>Explore</h4>
+                <h4>{heading}</h4>
             </div>
-            <br />
-            <div className="row">
-                <h6>
-                    Mission
-                </h6>
-            </div>
-            <div className="row">
-                <h6>
-                    Clubs and Teams
-                </h6>
-            </div>
-            <div className="row">
-                <h6>
-                    Documentations
-                </h6>
-            </div>
-            <div className="row">
-                <h6>
-                    Careers
-                </h6>
-            </div>
+            {objects.map(function (object, i) {
+                return (
+                    <div className="row">
+                        <h6>
+                            {object}
+                        </h6>
+                    </div>
+                );
+            })}
         </div>
     )
 }
 
-function Group2 (props) {
+function Group2(props) {
     return (
-        <div className = "group2">
+        <div className="group2">
             <div className="row">
                 <h4>
                     Developed by
@@ -60,20 +50,20 @@ function Group2 (props) {
     )
 }
 
-function Group3 (props) {
+function Group3(props) {
     return (
         <div className="group3">
             <div className="row">
-            <div className="col">
-                <h4>
-                    Tech@IITB
+                <div className="col">
+                    <h4>
+                        Tech@IITB
                 </h4>
                 </div>
             </div>
             <div className="row">
                 <div className="col">
-                <h6>
-                    All Rights Reserved
+                    <h6>
+                        All Rights Reserved
                 </h6>
                 </div>
             </div>
@@ -81,30 +71,29 @@ function Group3 (props) {
     )
 }
 
-function Group4 (props) {
+function Group4(props) {
+    var heading = props.heading;
+    var objects = props.content;
     return (
         <div className="group4">
             <div className="row">
                 <div className="col">
-                <h4>
-                    Contact
-                </h4>
+                    <h4>{heading}</h4>
                 </div>
             </div>
             <div className="row">
-                <div className="col">
-                    <h6>
-                        Facebook
-                    </h6>
-                </div>
-                <div className="col">
-                    <h6>
-                        Instagram
-                    </h6>
-                </div>
+                {objects.map(function (object, i) {
+                    return (
+                        <div className="col">
+                            <h6>
+                                {object}
+                            </h6>
+                        </div>
+                    );
+                })}
             </div>
         </div>
     )
 }
 
-export {Group1, Group2, Group3, Group4};
+export { Group1, Group2, Group3, Group4 };
