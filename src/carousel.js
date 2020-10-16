31a922b2d7e5db8ed5005480d322ad66e8e0324c
Kushal Kejriwal <kushalk333@gmail.com>
2020-10-16 22:26:35 +0530

    Organised Code, Added Basic Carousel

---
+++ src/carousel.js
@@ -0,0 +1,36 @@
+const data = [
+    {
+        img :"https://www.tech-iitb.org/static/team2020/aerohome-1.png",
+        heading: "First slide label",
+        sub: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+    },
+    {
+        img :"https://www.tech-iitb.org/static/team2020/erchome-1.png",
+        heading: "Second slide label",
+        sub: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+    },
+    {
+        img :"https://www.tech-iitb.org/static/team2020/wncchome-1.png",
+        heading: "Third slide label",
+        sub: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+    },
+    {
+        img :"https://www.tech-iitb.org/static/team2020/erchome-2.png",
+        heading: "Fourth slide label",
+        sub: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+    },
+    {
+        img :"https://www.tech-iitb.org/static/team2020/aerohome-2.png",
+        heading: "Fifth slide label",
+        sub: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+    },
+    {
+        img :"https://www.tech-iitb.org/static/team2020/wncchome-2.png",
+        heading: "Sixth slide label",
+        sub: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+    }
+
+]
+
+
+export default data;
