c95dd5350d0374a14c1e7acbf557d7bad62b315f
Kushal Kejriwal <kushalk333@gmail.com>
2020-10-16 22:26:48 +0530

    CardGroup

--- src/components/CardGroup.jsx
+++ src/components/CardGroup.jsx
@@ -4,16 +4,16 @@ import Card from "./Card";
 
 function CardGroup(){
     return(
-        <div class = "container">
-        <div className = "row">   
-        {techteams.map(team => (
-            <Card 
-            heading = {team.heading}
-            subheading = {team.subheading}
-            description = {team.description}
-            img = {team.img} / >
-        ))} 
-        </div>
+        <div class="container">
+            <div className="row">
+                {techteams.map(team => (
+                    <Card
+                        heading={team.heading}
+                        subheading={team.subheading}
+                        description={team.description}
+                        img={team.img} />
+                ))}
+            </div>
         </div>
     )
 }
