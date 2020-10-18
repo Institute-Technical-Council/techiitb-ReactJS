8ae2a62e7a1e931452a5cf196aa33cc3e44b2fd4
VishnuSanjevThulasiraman <52427253+VishnuSanjevThulasiraman@users.noreply.github.com>
2020-10-19 04:39:50 +0530

    Code formatting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../style/footer.css'
-import {Group1, Group2, Group3, Group4} from '../components/FooterGroups'
+import { Group1, Group2, Group3, Group4 } from '../components/FooterGroups'
 
 function Footer(props) {
     var heading_of_group1 = "Explore";
@@ -8,27 +8,27 @@ function Footer(props) {
     var heading_of_group4 = "Contact";
     var content_of_group4 = ["Facebook", "Instagram", "LinkedIn"];
     return (
-
-        <div className="row footer">
-            <div className="col-md-3 sm-3">
-            <h1>ITC Logo</h1>
-            </div>
-            <div className="col-md-3 sm-3">
-                <Group1 heading={heading_of_group1} content={content_of_group1}/>
-            </div>
-            <div className="col-md-3 sm-3">
-                <Group2 />
-            </div>
-            <div className="col-md-3 sm-3">
-                <div className="row">
-                    <Group3 />
+        <div className="container-fluid">
+            <div className="row footer">
+                <div className="col-md-3 sm-3">
+                    <h1>ITC Logo</h1>
                 </div>
-                <div className="row">
-                    <Group4 heading={heading_of_group4} content={content_of_group4}/>
+                <div className="col-md-3 sm-3">
+                    <Group1 heading={heading_of_group1} content={content_of_group1} />
+                </div>
+                <div className="col-md-3 sm-3">
+                    <Group2 />
+                </div>
+                <div className="col-md-3 sm-3">
+                    <div className="row">
+                        <Group3 />
+                    </div>
+                    <div className="row">
+                        <Group4 heading={heading_of_group4} content={content_of_group4} />
+                    </div>
                 </div>
             </div>
         </div>
-
     )
 }
 
