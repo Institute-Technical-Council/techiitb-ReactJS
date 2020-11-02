01577bb1a57e08e17ac76bc8c62aa7729b9f657c
VishnuSanjevThulasiraman <52427253+VishnuSanjevThulasiraman@users.noreply.github.com>
2020-11-02 13:30:38 +0530

    added navbar in app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import Footer from "./Footer";
 import CarouselHome from "./Carousel";
 import CardGroup from "./CardGroup"
-
+import NavBar from "./NavBar";
+import itc from "../assets/itcwhite.png"
 
 function App() {
     return (
         <div>
-            <CarouselHome />
+            <CarouselHome />  
+            <NavBar nav_items={["Home","Documentations","Hall of Fame"]} present={"Home"} logo_src={itc}/>
             <CardGroup />
             <Footer />
         </div>
