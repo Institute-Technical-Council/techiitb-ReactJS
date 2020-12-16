71d8390ff6b728a42667fe7185f71940fa0877fb
niharikamaheshwary <65452922+niharikamaheshwary@users.noreply.github.com>
2020-12-17 04:05:17 +0530

    Team 2020

---
+++ src/team_2.js
@@ -0,0 +1,26 @@
+const team_2 = [
+    {
+        heading : "Projects and Tech Nominee",
+        subheading : "R. Nambilakshmi" ,
+
+        img : "https://www.tech-iitb.org/static/team2020/RNambilakshmi.jpeg",
+    },
+    {
+        heading: "UG Nominee",
+        subheading: "Abhijeet Verma",
+        img: "https://www.tech-iitb.org/static/team2020/averma.png",
+
+
+    },
+    {
+        heading: "PG Nominee",
+        subheading: "Ashish Pandit",
+        img: "https://www.tech-iitb.org/static/team2020/ashish.png",
+    },
+    {
+        heading: "Manager Chemistry",
+        subheading: "Sanjyot Shenoy",
+        img: "https://www.tech-iitb.org/static/team2020/sanjyot.png",
+    },
+    ];
+export default team_2;
