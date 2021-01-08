10535e591861ad1fa3b25d2adb02055865a3b58b
VishnuSanjevThulasiraman <52427253+VishnuSanjevThulasiraman@users.noreply.github.com>
2021-01-08 20:59:34 +0530

    link added with reference

--- src/components/BlogsCardsGroup.jsx
+++ src/components/BlogsCardsGroup.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 import blogs from "../blogs"
 import BlogCards from "./BlogsCards";
 import { ParallaxProvider, Parallax } from "react-skrollr";
@@ -14,29 +13,31 @@ const data = {
     "data-top-top": "padding: 6px;",
     "data-bottom-bottom": "padding: 2px;",
     "data-center-bottom": "padding: 100px;"
-  };
-function BlogCardsGroup(props){
-    return(
-       
+};
+function BlogCardsGroup(props) {
+    return (
+
         <div className="container-fluid">
-        <div class = "jumbotron rounded" style = {{backgroundColor : "white"}} >
-            <h1 class = "display-4" style = {{color: "#0B2D5C" ,fontFamily: 'Bebas Neue', textAlign: 'center'}}> {props.title}</h1>
+            <div class="jumbotron rounded" style={{ backgroundColor: "white" }} >
+                <h1 class="display-4" style={{ color: "#0B2D5C", fontFamily: 'Bebas Neue', textAlign: 'center' }}> {props.title}</h1>
 
 
-            <div className="row">
+                <div className="row">
 
-                {props.group.map(blog => (
-                    <BlogCards
-                        heading={blog.title}
-                        subheading={blog.author+"/"+blog.category}
-                        description={blog.short_description}
-                        img={blog.cover_img} />
-                ))}
+                    {props.group.map(blog => (
+                        <BlogCards
+                            heading={blog.title}
+                            subheading={blog.author + "/" + blog.category}
+                            description={blog.short_description}
+                            img={blog.cover_img} 
+                            link={blog.link}
+                            />
+                    ))}
 
+                </div>
             </div>
         </div>
-        </div>
-       
+
     )
 }
 
