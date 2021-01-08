6fc4d251866ff9f161d2229f21b523c418b5fb80
VishnuSanjevThulasiraman <52427253+VishnuSanjevThulasiraman@users.noreply.github.com>
2021-01-08 20:59:07 +0530

    added blogs card content

---
+++ src/blogs.js
@@ -0,0 +1,18 @@
+
+import sos from "./assets/sos.jpg"
+import blog1 from "./assets/A1_6.jpg"
+
+
+const certi_main = [
+    
+    {
+        title: "My story" ,
+        author: "Rahul Prajapat" ,
+        category: "Article",
+        short_description: "I wanted to invent new things. That was my biggest motivation to do Tech stuff. Leading a change in the society and industry seemed much easier and doable through technology and innovation as compared to other things, say, like politics.",
+        cover_img: blog1,
+        link: "https://www.tech-iitb.org/blogs/1"
+    },
+
+    ];
+export default certi_main;
