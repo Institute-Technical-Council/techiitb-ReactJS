7dc5a9f5a866e741a6209b4dc2a4d1ffe664a53c
Kushal Kejriwal <kushalk333@gmail.com>
2021-01-13 23:58:44 +0530

    Blogs Integration

--- src/components/IndividualBlog.jsx
+++ src/components/IndividualBlog.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import '../style/individualblog.css'
 
-function IndividualBlog() {
+function IndividualBlog({match}) {
+
+    useEffect(() => {
+        fetchItem();
+        console.log(match);
+        
+        
+    },[]);
+    // 
+    const [item,setItem] = useState({});
+
+    const fetchItem = async () => {
+        const fetchItem = await fetch(`https://www.tech-iitb.org/api/v1/blogs/`);
+        const data = await fetchItem.json();
+        const author = match.params.author
+        console.log(author)
+        var i,j;
+        for (i = 0; i < data.length; i++) {
+            if (author == data[i].author) {
+                console.log("Matched")
+                j = i
+            }
+            // console.log(data[i].author)
+          }
+        console.log(data[j])
+        const item = data[j]
+        setItem(item)
+
+
+        
+
+         ;
+    }
     return (
         <div class = "row">
             <div class = "col-md-2 navigation">
@@ -20,14 +52,15 @@ function IndividualBlog() {
             </div>
             <div class = "col-md-10 blog">
                 <div class = "main container">
-                    <h3>TOPIC OF THE BLOG</h3>
-                    <img src = 'https://picsum.photos/400/400' height = "200px"></img>
-                    <h4> Donald Trump</h4>
-                    <h5> IITB 2000</h5>
-                    <p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                    <h3>{item.title}</h3>
+                    <img src = {item.cover_img} height = "200px"></img>
+                    <h4> {item.author}</h4>
+                    <h5> {item.short_description}</h5>
+                    {/* <p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
                     <p> Sed ut perspiciatis unde omnis ist natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
                     <p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                    
+                     */}
+                     <p> {item.content}</p>
 
                                     </div>
             </div>
